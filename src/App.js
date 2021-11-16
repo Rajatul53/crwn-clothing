6b604a39fe484d5c8dcda2e128ec77ee33a5c84f
Rajatul53 <rajatul53@gmail.com>
2021-11-16 15:35:53 +0530

    Added navigation using withRoute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import {  BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
-import Homepage from './homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <BrowserRouter>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </BrowserRouter>
     </div>
   );
 }
