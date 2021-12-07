bb594b90188745b125c883cb0de1ba6ad7c04db0
Rajatul53 <rajatul53@gmail.com>
2021-12-07 22:51:21 +0530

    added header and link functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {  BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -9,7 +9,9 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+
       <BrowserRouter>
+        <Header />
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
       </BrowserRouter>
